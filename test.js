a7369c149a17ad6695d95bd8bd9244e3f14f363f
tang <642568902@qq.com>
2021-03-26 17:23:27 +0800

    修改

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const http = require('http')
 
 let server = http.createServer((res, req) => {
   console.log(res.url)
+  req.wirth('链接成功')
+  req.end()
 }).listen(8088, () => {
   console.log('listen sucess on 8088')
 })
